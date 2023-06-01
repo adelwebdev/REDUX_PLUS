30b470e371b115ff696eded4be462d3a2223e29b
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2023-06-01 20:22:15 +0300

    Add files via upload

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Like from "./Like";
+import { isEmpty } from "./Utils";
 
 const Post = ({ post }) => {
   const [editToggle, setEditToggle] = useState(false);
+  const user = useSelector((state) => state.userReducer);
+  const [editContent, setEditContent] = useState(post.content);
+  const dispatch = useDispatch();
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    const postData = {
+      title: post.title,
+      author: user.pseudo,
+      likes: post.likes,
+      id: post.id,
+      content: editContent,
+    };
+    dispatch(editContent(postData));
+    setEditToggle(false);
+  };
 
   return (
     <div className="post">
-      <div className="edit-delete">
-        <img
-          src="./icons/edit.svg"
-          alt="edit"
-          onClick={() => setEditToggle(!editToggle)}
-        />
-        <img
-          src="./icons/delete.svg"
-          alt="delete"
-        />
-      </div>
+      {!isEmpty(user) && user.pseudo === post.author && (
+        <div className="edit-delete">
+          <img
+            src="./icons/edit.svg"
+            alt="edit"
+            onClick={() => setEditToggle(!editToggle)}
+          />
+          <img src="./icons/delete.svg" alt="delete" />
+        </div>
+      )}
 
       <h2>{post.title}</h2>
       <img
@@ -26,8 +43,12 @@ const Post = ({ post }) => {
       />
 
       {editToggle ? (
-        <form>
-          <textarea autoFocus={true} defaultValue={post.content}></textarea>
+        <form onSubmit={(e) => handleEdit(e)}>
+          <textarea
+            autoFocus={true}
+            defaultValue={post.content}
+            onChange={(e) => setEditContent(e.target.valur)}
+          ></textarea>
           <input type="submit" value="Valider modification" />
         </form>
       ) : (
